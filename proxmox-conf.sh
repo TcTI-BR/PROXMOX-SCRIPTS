1b3dcbcd0e0a71642d753de2f9f6c59a191f60b8
TcTI-BR <111935592+TcTI-BR@users.noreply.github.com>
2023-02-12 15:03:08 -0300

    Update proxmox-conf.sh

--- proxmox-conf.sh
+++ proxmox-conf.sh
@@ -5,10 +5,14 @@
 version=V002.R001
 # changelog
 
-echo -e "\033[33m Atenção! O uso do script fornecido é de inteira responsabilidade do utilizador. A pessoa ou empresa que forneceu o script não será responsável por quaisquer \033[31mproblemas ou danos causados\033[33m pelo uso do mesmo.\033[0m"
-echo -e "\033[33m Antes de utilizar o script, é importante que você faça uma avaliação cuidadosa e compreenda as implicações do seu uso. \033[31mCertifique-se de que o script é seguro e adequado\033[33m para as suas necessidades antes de utilizá-lo.\033[0m"
-echo -e "\033[33m Em resumo, \033[31mutilize o script por sua conta e risco\033[33m. A pessoa ou empresa que forneceu o script não será responsável por quaisquer problemas ou danos causados pelo seu uso. \033[0m"
-echo -e "\033[33mPressione uma tecla para continuar...\033[0m"
+clear
+echo -e "\033[33m Atenção! O uso do script fornecido é de inteira responsabilidade do utilizador.\n A pessoa ou empresa que forneceu o script não será responsável por quaisquer\n \033[31mproblemas ou danos causados\033[33m pelo uso do mesmo.\033[0m"
+echo -e " "
+echo -e "\033[33m Antes de utilizar o script, é importante que você faça uma avaliação cuidadosa e\n compreenda as implicações do seu uso. \033[31mCertifique-se de que o script é\n seguro e adequado\033[33m para as suas necessidades antes de utilizá-lo.\033[0m"
+echo -e " "
+echo -e "\033[33m Em resumo, \033[31mutilize o script por sua conta e risco\033[33m. A pessoa ou\n empresa que forneceu o script não será responsável por quaisquer problemas ou danos causados\n pelo seu uso. \033[0m"
+echo -e " "
+echo -e "\033[33mAo pressionar uma tecla você concorda com os riscos...\033[0m"
 read -p  " "
 clear
 
